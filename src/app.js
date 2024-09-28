f443476974384506411f509c15b5f3f952f02582
AdrielD23 <162411904+AdrielD23@users.noreply.github.com>
2024-09-28 06:21:46 +0000

    to main

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let icons = [
+    { key: "♦", color: "red" },
+    { key: "♥", color: "red" },
+    { key: "♠", color: "black" },
+    { key: "♣", color: "black" }
+  ];
+
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.querySelector(".top-suit").innerHTML = randomIcon.key;
+  document.querySelector(".top-suit").style.color = randomIcon.color;
+  document.querySelector(".bottom-suit").innerHTML = randomIcon.key;
+  document.querySelector(".bottom-suit").style.color = randomIcon.color;
+  document.querySelector(".number").innerHTML = randomNumber;
 };
